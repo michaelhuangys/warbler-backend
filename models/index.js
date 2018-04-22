9b179a274e6ed2302591c510525a4aa6e278d7ae
huang <yhuang@mai.sfsu.edu>
2018-04-22 01:37:59 -0700

    first commit

---
+++ models/index.js
@@ -0,0 +1,8 @@
+const mongoose=require('mongoose');
+mongoose.set('debug',true);
+mongoose.connect('mongodb://localhost/warbler',{
+	keepAlive:true
+})
+
+module.exports.User=require('./user');
+module.exports.Message=require('./message');
